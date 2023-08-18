98ba2bef5238f7c1910851ff03522057ffbd6584
Selim <angelica.selim@fujitsu.com>
2023-08-18 13:55:59 +0800

    update the api url

--- frontend/src/baseURL.js
+++ frontend/src/baseURL.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://mern-azure-web-app.azurewebsites.net";
+const baseUrl = "https://mern-azure-web-app.azurewebsites.net/api";
 
 //const baseUrl = "http://localhost:5000/api"; 
 
